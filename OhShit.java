47fc0c266c04eff5623347e1bc390d9b7a3c032f
KelOtt92 <kelvynottolini@gmail.com>
2025-02-21 11:04:43 -0300

    COMMIT inicial

---
+++ OhShit.java
@@ -0,0 +1,32 @@
+﻿import java.util.*;
+import java.lang.Math;
+
+class Program {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        // Sistema de informação SESI
+        System.out.println("Portal do Aluno");
+
+        // Declaração de Variaveis
+        String nome;
+        int nota;
+
+        // Entradas
+        System.out.println("Informe o Nome do Aluno");
+        nome = input.nextLine();
+        System.out.println("Informe a Nota");
+        nota = input.nextInt();
+
+        // Processamento
+        if (nota >= 50) {
+
+            // Saída
+            System.out.println(nome + "Estudando Está aprovado");
+        } else {
+
+            // Saída
+            System.out.println(nome + "Estudante Não está aprovado");
+        }
+    }
+}
